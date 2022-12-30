42ed88c8e60f9071abdac55046f945667e54f44e
Artur Das <a.daschevici@gmail.com>
2022-12-30 15:58:06 +0100

    Add a silly test

--- projects/calculator/test_calculator.py
+++ projects/calculator/test_calculator.py
@@ -1,3 +1,4 @@
+import pytest
 from projects.calculator.calculator import Calculator
 
 
@@ -11,7 +12,6 @@ def test_works_always_2():
         assert calc.add(2, 3) == 1
 
 if __name__ == "__main__":
-    import pytest
     import sys
     print(f"Version {sys.version}")
     raise SystemExit(pytest.main([__file__]))
