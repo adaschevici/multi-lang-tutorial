a905fe8367751ff352c0889b5e207aa16d15a22a
Artur Das <a.daschevici@gmail.com>
2022-12-30 15:56:24 +0100

    Add a silly test

--- projects/calculator/test_calculator.py
+++ projects/calculator/test_calculator.py
@@ -7,22 +7,11 @@ def test_works_always():
 
 def test_works_always_2():
     calc = Calculator()
-    assert calc.add(2, 3) == 1
+    with pytest.raises(AssertionError):
+        assert calc.add(2, 3) == 1
 
 if __name__ == "__main__":
     import pytest
     import sys
     print(f"Version {sys.version}")
     raise SystemExit(pytest.main([__file__]))
-
-# import unittest
-# from projects.calculator.calculator import Calculator
-# 
-# class TestSum(unittest.TestCase):
-#     
-#     def test_sum(self):
-#         calc = Calculator()
-#         self.assertEqual(calc.add(2, 3), 3)
-# 
-# if __name__ == "__main__":
-#     unittest.main()
