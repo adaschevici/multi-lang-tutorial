0ee078217ad82436c9fcfcc4d6453f0e2a31a4f7
Artur Das <a.daschevici@gmail.com>
2023-01-02 09:23:39 +0100

    Add golang library and simple web app that consumes it

---
+++ projects/go_hello_world/hello_world.go
@@ -0,0 +1,6 @@
+package go_hello_world
+
+
+func HelloWorld() string {
+    return "Hello World!"
+}
---
+++ projects/go_hello_world/hello_world_test.go
@@ -0,0 +1,12 @@
+package go_hello_world
+
+import "testing"
+
+func TestGreeter(t *testing.T) {
+  expected := "Hello World!"
+  actual := HelloWorld()
+  if actual != expected {
+    t.Error("expected %q but got %q", expected, actual)
+  }
+}
+
